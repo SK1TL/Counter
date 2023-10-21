9402c7626cfe107ab50d22050223aef90cb9d5d4
Artur <SSSK1TLLL@yandex.ru>
2023-10-21 13:43:57 +0300

    Усложненная задача по спринту 3. 1. Убраны лишние строчки кода (коментарии) 2. Заведена переменная формата Date и константа формата DateFormatter. Также создана константа отображения даты. 3. В Main созданы дополнительные кнопки "+", "-", "Сброс" и поле "TextView". 4. Заведены настройки для отображения даты и времени, а также заведены настройки отображения Истории изменений счётчика. 5 и 6. Записаны функции работы кнопок приложения согласно заданию.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -1,29 +1,48 @@
-//
-//  ViewController.swift
-//  Counter
-//
-//  Created by Артур Гайфуллин on 16.10.2023.
-//
-
 import UIKit
 
+var todayDate = Date()
+let dateFormatter = DateFormatter()
+let dateInfo = dateFormatter.string(from: todayDate)
+
 class ViewController: UIViewController {
-    
-    var count = 0
-    
-    @IBOutlet weak var counterInfo: UILabel!
+    private var count = 0
+
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var historyText: UITextView!
+    @IBOutlet weak var increaseButton: UIButton!
+    @IBOutlet weak var decreaseButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .medium
+        dateFormatter.locale = Locale(identifier: "ru_RU")
+        historyText.isEditable = false
+        historyText.isScrollEnabled = true
     }
 
-    func countInfo() {
+    @IBAction private func increaseOne(_ sender: Any) {
         count += 1
-        counterInfo.text = "Значение счетчика: \(count)"
+        counterLabel.text = "Значение счётчика: \(count)"
+        historyText.text += "\n \(dateInfo): значение изменено на +1"
     }
-
-    @IBAction func counterButton(_ sender: Any) {
-        countInfo()
+    
+    @IBAction private func decreaseOne(_ sender: Any) {
+        if count > 0 {
+            count -= 1
+            counterLabel.text = "Значение счётчика: \(count)"
+            historyText.text += "\n \(dateInfo): значение изменено на -1"
+        } else {
+            historyText.text += "\n \(dateInfo): попытка уменьшить значение счётчика ниже 0"
+        }
+        
+    }
+    
+    @IBAction private func resetAll(_ sender: Any) {
+        count = 0
+        counterLabel.text = "Значение счётчика: \(count)"
+        historyText.text += "\n \(dateInfo): значение сброшено"
     }
 }
 
