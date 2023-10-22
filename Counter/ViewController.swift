4a889b4465f1b87f0baa310291e5f977807bc369
Artur <SSSK1TLLL@yandex.ru>
2023-10-22 19:55:22 +0300

    Приведено к более красивому виду

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -1,37 +1,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-    private var count = 0
-
-    let dateFormatter = DateFormatter()
     
-    @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var historyTextView: UITextView!
-    @IBOutlet weak var increaseButton: UIButton!
-    @IBOutlet weak var decreaseButton: UIButton!
-    @IBOutlet weak var resetButton: UIButton!
+    private var count = 0
+    private var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .medium
+        formatter.locale = Locale(identifier: "ru_RU")
+        return formatter
+    }
+    
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet private weak var historyTextView: UITextView!
+    @IBOutlet private weak var increaseButton: UIButton!
+    @IBOutlet private weak var decreaseButton: UIButton!
+    @IBOutlet private weak var resetButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        dateFormatter.dateStyle = .short
-        dateFormatter.timeStyle = .medium
-        dateFormatter.locale = Locale(identifier: "ru_RU")
         historyTextView.isEditable = false
         historyTextView.isScrollEnabled = true
     }
     
     private func dateString() -> String {
-        var todayDate = Date()
-        return dateFormatter.string(from: todayDate)
+        dateFormatter.string(from: Date())
     }
 
-    @IBAction private func increaseOne(_ sender: Any) {
+    @IBAction private func increaseButtonTapped(_ sender: Any) {
         count += 1
         counterLabel.text = "Значение счётчика: \(count)"
         historyTextView.text += "\n \(dateString()): значение изменено на +1"
     }
     
-    @IBAction private func decreaseOne(_ sender: Any) {
+    @IBAction private func decreaseButtonTapped(_ sender: Any) {
         if count > 0 {
             count -= 1
             counterLabel.text = "Значение счётчика: \(count)"
@@ -39,13 +41,11 @@ class ViewController: UIViewController {
         } else {
             historyTextView.text += "\n \(dateString()): попытка уменьшить значение счётчика ниже 0"
         }
-        
     }
     
-    @IBAction private func resetAll(_ sender: Any) {
+    @IBAction private func resetButtonTapped(_ sender: Any) {
         count = 0
         counterLabel.text = "Значение счётчика: \(count)"
         historyTextView.text += "\n \(dateString()): значение сброшено"
     }
 }
-
